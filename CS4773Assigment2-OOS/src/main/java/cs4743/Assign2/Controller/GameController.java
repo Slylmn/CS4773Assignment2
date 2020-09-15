28936cf18ff1023463a71dea91c3f3bc5bf79caf
Isaias A. Monster <isaiasmtz12@yahoo.com>
2020-09-15 10:32:59 -0500

    added fxml and fxml

--- CS4773Assigment2-OOS/src/main/java/cs4743/Assign2/Controller/GameController.java
+++ CS4773Assigment2-OOS/src/main/java/cs4743/Assign2/Controller/GameController.java
@@ -1,15 +1,36 @@
 package cs4743.Assign2.Controller;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
 public class GameController implements Initializable{
 
 	//play game
 	
-	
+	    @FXML
+	    private TextArea gameOutput;
+
+	    @FXML
+	    private Button playWar;
+
+	   // private War war;  john needs to code this class
+
+	   /* public GameController(War war) {
+	        this.war = war;
+	    }
+	    */
+
+	    @FXML
+	    void playWar(ActionEvent event) {
+	        // TODO: play a game of War (i.e., until someone wins or there is a tie)
+	    }
 	
 	
 	
